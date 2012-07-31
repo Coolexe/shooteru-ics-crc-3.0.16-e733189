c5e20b17d4df5996811c0fda2be1b05d99952a34
Coolexe <kimithemes@gmail.com>
2012-07-31 21:27:28 +0530

    updating

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -912,12 +912,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 266667000,
