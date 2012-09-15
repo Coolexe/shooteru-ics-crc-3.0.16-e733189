542b4f3a3708e81f688793e2a2abdc60f00e39ae
Coolexe <kimithemes@gmail.com>
2012-09-15 15:16:52 +0530

    Revert "Fsync Added"

--- fs/sync.c
+++ fs/sync.c
@@ -25,10 +25,6 @@
 extern bool early_suspend_active;
 #endif
 
-#ifdef CONFIG_FSYNC_CONTROL
-extern bool fsynccontrol_fsync_enabled(void);
-#endif
-
 /*
  * Do the filesystem syncing work. For simple filesystems
  * writeback_inodes_sb(sb) just dirties buffers with inodes so we have to
@@ -147,11 +143,6 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 	int fput_needed;
 
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
-
 	file = fget_light(fd, &fput_needed);
 	if (!file)
 		return -EBADF;
@@ -185,11 +176,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 #endif
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
-
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
@@ -226,12 +212,6 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
-
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -240,11 +220,6 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct file *file;
 	int ret = -EBADF;
-
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 
 	file = fget(fd);
 	if (file) {
@@ -261,10 +236,6 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 		return 0;
 	else
 #endif
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 	return do_fsync(fd, 0);
 }
 
@@ -274,10 +245,6 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 	if (!early_suspend_active)
 		return 0;
 	else
-#endif
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
 #endif
 	return do_fsync(fd, 1);
 }
@@ -292,10 +259,6 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
 		return 0;
 	return vfs_fsync_range(file, pos, pos + count - 1,
@@ -364,11 +327,6 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	loff_t endbyte;			/* inclusive */
 	int fput_needed;
 	umode_t i_mode;
-
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
